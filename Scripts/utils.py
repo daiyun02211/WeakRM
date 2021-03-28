b3412234f2228542792d514c280d0170489f4eaf
daiyun02211 <daiyun.huang@outlook.com>
2021-03-28 11:00:28 +0800

    update

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -26,14 +26,14 @@ def embed(sequence, instance_len, instance_stride, zero_pad=True):
                instance_stride + instance_len - len(sequence))
     left_pad = int(gap / 2)
     right_pad = gap - left_pad
-    padded_seq = np.pad(sequence, (left_pad, right_pad), 'constant', constant_values=5)
+    padded_seq = np.pad(sequence, (left_pad, right_pad), 'constant', constant_values=4)
     bag = []
     for i in np.arange(max(1, np.floor(instance_num)+(gap > 0))):
         instance = padded_seq[int(i*instance_stride):int(i*instance_stride+instance_len)]
         bag.append(instance)
     bag = np.stack(bag).astype(np.int32)
     one_hot_bag = np.eye(5)[bag][:, :, :4].astype(np.float32)
-    condition = (bag == 5).reshape(-1, instance_len, 1)
+    condition = (bag == 4).reshape(-1, instance_len, 1)
     if zero_pad:
         one_hot_bag = np.where(condition, np.zeros_like(one_hot_bag, dtype=np.float32), one_hot_bag)
     else:
